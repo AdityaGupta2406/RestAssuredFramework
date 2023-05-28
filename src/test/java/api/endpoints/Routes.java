f4bcb8f88fe4d98f4482b567348d94c12809e063
AdityaGupta2406 <agaditya7@gmail.com>
2023-05-28 21:38:15 +0530

    first commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,29 @@
+package api.endpoints;
+
+/*Base  URL : https://petstore.swagger.io/v2/
+Post : https://petstore.swagger.io/v2/user
+Post : /user/createWithList
+Get : /user/{username}
+put : /user/{username} 
+Delete : /user/{username} 
+Get : /user/login 
+Get : /user/logout 
+Post : /user/logout 
+*/
+public class Routes {
+	
+	public static String base_url="https://petstore.swagger.io/v2";
+	
+	
+	//user modules 
+	public static String post_url=base_url+"/user";
+
+	public static String get_url="";
+
+	public static String update_url="";
+
+	public static String delete_url="";
+
+	
+
+}
